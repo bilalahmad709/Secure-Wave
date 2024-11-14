6da2e80e0963a38e61c4a210805486af9a430de0
Bilal Ahmad <bilalmomand709@gmail.com>
2024-11-14 18:46:28 +0500

    added js file

---
+++ Assets/Pages/secure_wave.js
@@ -0,0 +1,25 @@
+// Navigation bar start
+
+//Home
+let homeLink = document.getElementById("home");
+homeLink.innerText="Home";
+
+//services
+let serviceslink = document.getElementById("services").innerText="Services";
+
+//solution
+let solutionlink = document.getElementById("solutions").innerText= "Solutions";
+
+//Resources
+let resources = document.getElementById("resources").innerText= "Resources";
+
+// About Us
+let about_usLink = document.getElementById("about_us").innerText= "About Us";
+
+//Contact Us
+let contactLink = document.getElementById("contact_us").innerText="Contact Us";
+
+// adding hover class for navlinks
+
+
+//navigation bar end
